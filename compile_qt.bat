4b881a572b436d8e08f4cf353a52e1e5f0e74d7e
Constantin Wenger <constantin.wenger@googlemail.com>
2015-04-28 20:30:54 +0200

    compile qt batch script

---
+++ compile_qt.bat
@@ -0,0 +1,1 @@
+C:\Python34\python C:\Python34\Lib\site-packages\PySide\scripts\uic.py F:\source\git\PveLauncher\gui\qt\generated\account_dialog.ui -o F:\source\git\PveLauncher\gui\qt\generated\account_dialog.py
