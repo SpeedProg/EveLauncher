eb36d89b2651dcb6581a3a2e8d6e6fc80d3cff94
Constantin Wenger <constantin.wenger@googlemail.com>
2015-06-13 11:55:05 +0200

    using wrong member name for URLError to get error msg

--- PveLauncher.py
+++ PveLauncher.py
@@ -127,7 +127,7 @@ class QtStarter():
             window.set_server_status(status_text, status.online_players)
 
         except URLError as e:
-            window.set_server_status(e.msg, 0)
+            window.set_server_status(e.reason, 0)
 
         self.timer = threading.Timer(10.0, self.update_server_status, kwargs={'window': window, 'api': api})
         self.timer.start()
